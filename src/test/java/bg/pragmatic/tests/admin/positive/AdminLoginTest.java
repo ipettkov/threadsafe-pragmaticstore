980d0e3ff24a290bc6673caf295bdc2fcaf5e8df
Ivan Petkov <ipettkov@gmail.com>
2020-03-24 00:02:39 +0200

    fix

--- src/test/java/bg/pragmatic/tests/admin/positive/AdminLoginTest.java
+++ src/test/java/bg/pragmatic/tests/admin/positive/AdminLoginTest.java
@@ -3,7 +3,7 @@ package bg.pragmatic.tests.admin.positive;
 import bg.pragmatic.core.core.App;
 import enums.Password;
 import enums.Username;
-import org.testng.Assert;
+import static org.testng.Assert.*;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -26,7 +26,7 @@ public class AdminLoginTest {
     @Test
     public void successfulLoginTest() {
         app.adminLoginPage.login(Username.VALID_USERNAME, Password.ADMIN_PASSWORD);
-        Assert.assertEquals(app.adminDashBoardPage.isLogoutButtonDisplayed(), true, "The logout button is not displayed");
+        assertTrue(app.adminDashBoardPage.isLogoutButtonDisplayed(), "The logout button is not displayed");
         app.adminDashBoardPage.logout();
     }
 
